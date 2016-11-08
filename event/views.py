cfa3202a3a5b0a97440fe316e3ac35b8762298fc
iwarriorgit <warrior_xr@126.com>
2016-11-08 21:17:07 +0800

    merge

--- event/views.py
+++ event/views.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from event.models import Column, Event
 
 
 def index(request):
-    columns=Column.objects.all()
-    return render(request, 'event/index.html', {'columns':columns})
+    home_display_columns = Column.objects.filter(home_display=True)
+    nav_display_columns = Column.objects.filter(nav_display=True)
+    return render(request, 'event/index.html', {
+        'home_display_columns': home_display_columns,
+        'nav_display_columns': nav_display_columns,
+        'homepage': True,
+    })
 
 
 def column_detail(request, column_slug):
-    column=Column.objects.get(slug=column_slug)
-    return render(request, 'event/column.html',{'column':column})
+    column = Column.objects.get(slug=column_slug)
+    return render(request, 'event/column.html', {'column': column, 'homepage': False, })
 
 
-def event_detail(request, event_slug):
-    event=Event.objects.filter(slug=event_slug)[0]
-    return render(request, 'event/event.html',{'event': event})
+def event_detail(request, id, event_slug):
+    at_home = True
+    event = Event.objects.get(id=id)
+    if event_slug != event.slug:
+        return redirect(event, permanent=True)
+
+    return render(request, 'event/event.html', {'event': event,'homepage': False, })
