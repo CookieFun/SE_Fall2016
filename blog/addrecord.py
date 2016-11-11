248a4c26777771b6e83462455f57441d0fdf80cf
iwarriorgit <warrior_xr@126.com>
2016-11-11 18:56:52 +0800

    add record--step 5.1 :  trying to connect database

--- blog/addrecord.py
+++ blog/addrecord.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.utils.datastructures import MultiValueDictKeyError
 from django.http import HttpResponse
 import os
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.conf import settings
 
-
-# Create your views here.
+from blog.models import UploadFile
+from blog.forms import UploadFileForms
 
 
 def redirect(request):
@@ -34,13 +33,17 @@ def upload(request):
     if request.method == 'POST':
         print('file:')
         print(request)
-        if request.FILES is not None:
-            file = request.FILES.get('uploadFileName')
-            print('trying to save into disk...', file.name)
-            path = default_storage.save('tmp/'+file.name, ContentFile(file.read()))
-            tmp_file = os.path.join(settings.MEDIA_ROOT, path)
+        print('trying to save')
+        newfile = UploadFile(upload_file=request.FILES['uploadFileName'])
+        newfile.save()
+        # if request.FILES is not None:
+        #     file = request.FILES.get('uploadFileName')
+        #     print('trying to save into disk...', file.name)
+        #     path = default_storage.save('tmp/'+file.name, ContentFile(file.read()))
+        #     tmp_file = os.path.join(settings.MEDIA_ROOT, path)
         return HttpResponse('tmp_file')
     else:
+        form = UploadFileForms()
         print('empty')
         return HttpResponse('upload-fail')
 
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-# from django.db import models
-#
-#
-# class Document(models.Model):
-#     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+from django.db import models
+
+
+class UploadFile(models.Model):
+    upload_file = models.FileField(upload_to='upload_file/%Y/%m/%d')
